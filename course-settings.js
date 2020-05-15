3e6b85caa10b1e7842d201ded2e1afd1120ae7af
jknurminen <54582661+jknurminen@users.noreply.github.com>
2020-05-15 12:30:57 +0300

    Update course-settings.js

--- course-settings.js
+++ course-settings.js
@@ -1,9 +1,9 @@
 const courseSettings = {
-  language: "fi",
-  name: "Ohjelmoinnin MOOC 2020",
+  language: "en",
+  name: "Ethics of AI",
   siteUrl: "https://ohjelmointi-20.mooc.fi",
   githubUrl: "https://github.com/rage/ohjelmointi-20",
-  subtitle: "Ohjelmoinnin alkeet kaikille, ilmaiseksi",
+  subtitle: "The basics of ethical issues of AI for everybody",
   slug: "ohjelmoinnin-mooc-2020",
   tmcCourse: "2020-ohjelmointi",
   quizzesId: "a9251b4f-b3d2-4d0c-ab01-a143eb2834b5",
