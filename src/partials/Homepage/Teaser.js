a6750add6bb87d1f8ca318384bb82cfbf31efffa
iamgeorge <george.eyoyibo@gmail.com>
2020-10-19 13:13:50 +0300

    update Teaser and TeaserCard

--- src/partials/Homepage/Teaser.js
+++ src/partials/Homepage/Teaser.js
@@ -1,50 +1,16 @@
 import React from "react"
 import styled from "styled-components"
-import { useTrail, animated } from "react-spring"
 import { respond } from "../../_respond"
 import H3 from "../Headers/H3"
-import ChartIcon from "../../assets/chart-icon.svg"
-import PresenterIcon from "../../assets/presenter-icon.svg"
-import WeighIcon from "../../assets/weighbalance-icon.svg"
-import TeaserCard from "./TeaserCard"
 
-const obj = [
-  {
-    icon: <ChartIcon width="48px" height="48px" />,
-    title: "Question",
-    text: "Kesällä 2020 kurssi järjestetään ensimmäistä?",
-  },
-  {
-    icon: <PresenterIcon width="48px" height="48px" />,
-    title: "Question",
-    text: "Kesällä 2020 kurssi järjestetään ensimmäistä?",
-  },
-  {
-    icon: <WeighIcon width="48px" height="48px" />,
-    title: "Question",
-    text: "Kesällä 2020 kurssi järjestetään ensimmäistä?",
-  },
-]
-
-//TO-DO: Make Grid responsive without Media queries
-//TO-DO: + More Breakpoints
-const Wrapper = styled.div`
-  margin: 6rem 0;
-
-  span {
-    display: block;
-    padding: 0.8rem 0;
-    background: linear-gradient(160deg, #0093e9 0%, #80d0c7 100%);
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-    font-weight: 600;
-  }
+const TeaserWrapper = styled.div`
+  margin-top: 5rem;
 `
 
-const TeaserContainer = styled.div`
+const TeaserCardContainer = styled.div`
   padding: 2rem 0.5rem;
   margin-top: 2rem;
-  margin-bottom: 2rem;
+  margin-bottom: 4rem;
   width: 100%;
   display: grid;
   grid-template-columns: 1fr;
@@ -72,44 +38,17 @@ const TeaserContainer = styled.div`
 
   ${respond.lg`
   padding: 2rem 0;
-  grid-template-columns: repeat(3, 24rem);
+  grid-template-columns: repeat(3, 20rem);
   grid-gap: 6rem;
   `}
 `
 
 const TeaserQuestion = (props) => {
-  const trail = useTrail(obj.length, {
-    from: {
-      marginTop: -20,
-      opacity: 0,
-      transform: "translate3d(0, -40px, 0)",
-    },
-    to: {
-      marginTop: 20,
-      opacity: 1,
-      transform: "translate3d(0, 0, 0)",
-    },
-  })
-
   return (
-    <Wrapper>
-      <span> {props.subtitle} </span> <H3> {props.title} </H3>{" "}
-      <TeaserContainer>
-        {" "}
-        {trail.map((props, index) => {
-          return (
-            <animated.div key={obj[index]} style={props}>
-              <TeaserCard
-                index={index}
-                icon={obj[index].icon}
-                title={obj[index].title}
-                text={obj[index].text}
-              />{" "}
-            </animated.div>
-          )
-        })}{" "}
-      </TeaserContainer>{" "}
-    </Wrapper>
+    <TeaserWrapper>
+      <H3> {props.title} </H3> <span> {props.subtitle} </span>{" "}
+      <TeaserCardContainer> {props.children} </TeaserCardContainer>{" "}
+    </TeaserWrapper>
   )
 }
 
--- src/partials/Homepage/TeaserCard.js
+++ src/partials/Homepage/TeaserCard.js
@@ -1,62 +1,58 @@
 import React from "react"
+import ChartIcon from "../../assets/ethics-icon.svg"
+import PresenterIcon from "../../assets/phil-icon.svg"
+import WeighIcon from "../../assets/tech-icon.svg"
 import styled from "styled-components"
-import H5 from "../../partials/Headers/H5"
+
+const icons = {
+  chart: <ChartIcon width="60px" height="60px" />,
+  presenter: <PresenterIcon width="60px" height="60px" />,
+  weigh: <WeighIcon width="60px" height="60px" />,
+}
 
 const TeaserWrapper = styled.div`
   width: 100%;
+  height: 20rem;
   height: 16rem;
-  /*   box-shadow: ${(props) =>
+  box-shadow: ${(props) =>
     props.state
       ? `0 5px 10px rgba(154,160,185,.05), 0 15px 40px rgba(166,173,201,.2);`
-      : `0`}; */
-  border: 1px solid #e5e5e5;
-  border-radius: 6px;
-  display: grid;
-  align-content: center;
+      : `0`};
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   justify-content: center;
-  transition: 0.25s;
-
-  :hover {
-    background: #fff;
-    opacity: 1;
-    box-shadow: 0 10px 40px rgba(0, 0, 0, 0.2);
-    color: #202020;
-    border-color: transparent;
-  }
 `
 
 const TeaserIcon = styled.div`
-  width: 21.5%;
+  width: 100%;
   height: 65px;
-  display: grid;
+  display: flex;
   align-content: center;
   justify-content: center;
-  margin: 0.2rem auto 0.2rem auto;
+  margin-bottom: 0.5rem;
 `
 
 const TeaserText = styled.div`
   display: flex;
   flex-direction: column;
-  align-item: left;
+  align-item: center;
   padding: 0 1rem 0.5rem 1rem;
+  font-size: 0.9rem;
   font-weight: 400;
-  color: #202020;
-
-  p {
-    font-size: 16px;
-    line-height: 1.625;
-    font-family: "Merriweather", serif;
-    opacity: 0.8;
-  }
+  color: #6f7678;
 `
 
-export default ({ icon, index, title, text }) => {
+export default (props) => {
   return (
-    <TeaserWrapper>
-      <TeaserIcon> {icon} </TeaserIcon>{" "}
-      <TeaserText>
-        <H5> {title} </H5> <p>{text}</p>
-      </TeaserText>{" "}
-    </TeaserWrapper>
+    <>
+      <TeaserWrapper state={props.state}>
+        <div>
+          <TeaserIcon> {icons[props.icon]} </TeaserIcon>{" "}
+          <TeaserText> {props.children} </TeaserText>{" "}
+        </div>{" "}
+      </TeaserWrapper>{" "}
+    </>
   )
 }
