035841cb864d84ef50feb7fd851373554f82b64d
Henrik Nygren <nygrenh@gmail.com>
2020-11-27 11:05:07 +0200

    Google analytics tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -133,7 +133,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "G-SCZZR3LTX5",
+        trackingId: "UA-47575342-14",
         // Puts tracking script in the head instead of the body
         head: false,
         // Setting this parameter is optional
