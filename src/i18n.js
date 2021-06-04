8ea48c6bae2863d5a1863e74a24eb675fa84d045
Antti Leinonen <antti.leinonen@helsinki.fi>
2021-06-04 13:52:00 +0300

    Another try at preloading translations

--- src/i18n.js
+++ src/i18n.js
@@ -33,8 +33,7 @@ i18n.use(initReactI18next).init({
     wait: true,
   },
   lng: CourseSettings.language,
+  preload: ["common:en", "user:en", "points-balloon:en", "navbar:en"]
 })
-
-console.log(i18n.t("common:translationLoaded"))
 
 export default i18n
